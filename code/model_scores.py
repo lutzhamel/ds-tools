3d639b462823de8e8c3581d0a09c3155a947b59d
Lutz Hamel <lutz.hamel@gmail.com>
2022-01-08 22:59:51 -0500

    initial commit

---
+++ code/model_scores.py
@@ -0,0 +1,26 @@
+'''
+compute classification accuracy and R^2 scores together with the 95%
+confidence interval
+'''
+from sklearn.metrics import accuracy_score
+from confint import classification_confint
+
+def model_accuracy(model, X, y):
+  '''
+  compute classification accuracy together with the 95% confidence interval
+  Parameters:
+    model - estimator
+    X - independent features
+    y - target vector
+  Returns:
+    (acc,lb,ub) - accuracy, lowerbound, upperbound
+  '''
+  # compute the accuracy of optimal classifier      
+  predict_y = grid.best_estimator_.predict(X)
+  acc = accuracy_score(y, predict_y)
+
+  # 95% confidence interval
+  (lb,ub) = classification_confint(acc,X.shape[0])
+
+  # return a triple
+  return (acc,lb,ub)
